deb4865de66cdef09d938e970e6a424c80692349
gregsvieira <vieiragregory42@gmail.com>
2023-06-12 22:36:27 -0300

    react fragment ghost element html

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function App() {
-  return <h1>First Component App</h1>;
+  return (
+    <>
+      <h1>First Component App</h1>
+      <h2>Subtitle</h2>
+    </>
+  );
 }
 
 export default App;
