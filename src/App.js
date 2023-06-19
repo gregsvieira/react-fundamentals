9483de3f3b630b7101ece5fe7fa5801bb34262d3
gregsvieira <vieiragregory42@gmail.com>
2023-06-18 22:29:17 -0300

    using states

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import Post from './Post';
 import Header from './Header';
 
-const posts = [
-  { title: 'Title#01', subtitle: 'Subtitle#01', likes: 20 },
-  { title: 'Title#02', subtitle: 'Subtitle#02', likes: 30 },
-  { title: 'Title#03', subtitle: 'Subtitle#03', likes: 50 },
-];
+// const posts = ;
 
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: Math.random(), title: 'Title#01', subtitle: 'Subtitle#01', likes: 20 },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Subtitle#02', likes: 30 },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Subtitle#03', likes: 50 },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Subtitle#04', likes: 25 },
+  ]);
+
+  function handleRefreshClean(){
+    setPosts(() => []);
+  }
+
+  function handleRefresh() {
+    setPosts((prevState) => [
+      ...prevState,
+      {
+          id: Math.random(),
+          title: `Title#${posts.length + 1}`,
+          subtitle: `Subitle#${posts.length + 1}`,
+          likes: Math.random().toFixed(2) * 100
+        }
+    ]);
+  }
   return (
     <>
       <Header 
       
       subtitle="Deploy s of weeek">
-      <h3>{'Current Number:' + Math.random() * 100}</h3>
+      <h3>
+        {`Current Number: ${posts.length}`}
+      </h3>
+      <br/>
+      <button onClick={handleRefresh}> Atualizar </button>
+      <button onClick={handleRefreshClean}> Limpar </button>
       </Header>
 
       <hr />
 
       {posts.map(post => (
         <Post
-          key={post.title}
+          key={post.id}
           likes={post.likes}
           post={{
             title: post.title,
