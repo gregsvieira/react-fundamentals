254a48ad7b84efdc59494c3f585bf2b1591bfa5d
gregsvieira <vieiragregory42@gmail.com>
2023-06-19 22:06:24 -0300

    conditional render with if and ternary

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Header from './Header';
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Subtitle#01', likes: 20 },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Subtitle#02', likes: 30 },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Subtitle#03', likes: 50 },
-    { id: Math.random(), title: 'Title#04', subtitle: 'Subtitle#04', likes: 25 },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Subtitle#01', likes: 20, read: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Subtitle#02', likes: 30, read: true },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Subtitle#03', likes: 50, read: false },
+    { id: Math.random(), title: 'Title#04', subtitle: 'Subtitle#04', likes: 25, read: true },
   ]);
 
   function handleRefreshClean(){
@@ -52,13 +52,8 @@ function App() {
       {posts.map(post => (
         <Post
           key={post.id}
-          likes={post.likes}
           onRemove={handleRemovePost}
-          post={{
-            id: post.id,
-            title: post.title,
-            subtitle: post.subtitle,
-          }}
+          post={post}
         />
       ))}
 
