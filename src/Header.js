5b84e87d11cf425900f5e2d2151884e68f5e30e6
gregsvieira <vieiragregory42@gmail.com>
2023-06-25 23:35:07 -0300

    props drilling with Context Api

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from 'prop-types';
 import Button from "./Button";
-
+import { ThemeContext } from './ThemeContext';
 export default function Header(props) {
+  const { onToggleTheme } = useContext(ThemeContext);
+
   return (
     <>
     <h1>{props.title}</h1>
-    <Button theme={props.theme} onClick={props.onToggleTheme}>Change Theme</Button>
+    <Button onClick={onToggleTheme}>Change Theme</Button>
       <h2>{props.subtitle}</h2>
         {props.children}
     </>
@@ -14,8 +16,6 @@ export default function Header(props) {
 }
 
 Header.propTypes = {
-  theme: PropTypes.string.isRequired,
-  onToggleTheme: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   children: PropTypes.node,
