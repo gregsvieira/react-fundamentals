ba035dc1eb50a720d21daf6d3b769a92c4a0a21b
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-07-12 22:58:18 +0000

    themeProvider with styled componentes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Footer() {
+export default function Footer({ onToggleTheme, selectedTheme }) {
     return (
         <Container>
             <span>TaskListener. All rights reserved.</span>
-            <button type="button">🌞</button>
+            <button 
+                type="button" 
+                onClick={onToggleTheme}>
+                {selectedTheme === 'dark' ? '🌚' : '🌞'}
+            </button>
         </Container>
     )
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
   body {
-    background-color: #222;
-    color: #fff;
+    background-color: ${props => props.theme.backgroundColor};
+    color: ${props => props.theme.textColor};
     font-family: sans-serif;
   }
 `;
