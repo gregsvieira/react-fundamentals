fbaa4e7dd0f9b036fdd10532f60f9a44939eff46
Gregory Vieira <89167868+gregsvieira@users.noreply.github.com>
2023-08-05 11:01:49 +0000

    queryParams and pathParams on classComponents

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,17 +1,41 @@
 import React, { useMemo } from "react";
 import { useParams, useLocation } from "react-router-dom";
 
-export default function Post(){
+// export default function Post(){
 
-  const { id } = useParams();
-  const { search } = useLocation();
+//   const { id } = useParams();
+//   const { search } = useLocation();
   
-  const queryParams = useMemo(() => new URLSearchParams(search), [search])
+//   const queryParams = useMemo(() => new URLSearchParams(search), [search])
   
-  return(
-    <>
-      <h1>Post Page: {id} </h1>
-      <h2>Search: {queryParams.get('myQueryParam')}</h2>
-    </>
-  )
+//   return(
+//     <>
+//       <h1>Post Page: {id} </h1>
+//       <h2>Search: {queryParams.get('myQueryParam')}</h2>
+//     </>
+//   )
+// }
+
+
+export default class Post extends React.Component{
+  constructor(props){
+    super(props);
+    this.paramId = this.props.match.params.id;
+
+    const { search } = this.props.location;
+    this.queryParams = new URLSearchParams(search);
+  }
+
+  render() {
+    console.log(this.queryParams.get('myQueryParam'));
+
+    return (
+      <>
+        <h1>Post Page: {this.paramId}</h1>
+        <h2>
+          Search: {this.queryParams.get('myQueryParam')}
+        </h2>
+      </>
+    )
+  }
 }
